f70f0eb8e8d058653bdc9b3dd0c75855d37c77ca
Dilip Babu <106616107+619DILIP@users.noreply.github.com>
2024-08-16 15:37:11 -0400

    Update SeleniumTest.java

--- src/test/java/SeleniumTest.java
+++ src/test/java/SeleniumTest.java
@@ -24,7 +24,7 @@ public class SeleniumTest {
         System.setProperty("webdriver.chrome.driver", "./driver/chromedriver");
 
         // Get file
-        File file = new File("AsyncAwait.html");
+        File file = new File("src/main/AsyncAwait.html");
         String path = "file://" + file.getAbsolutePath();
 
         // Create a new ChromeDriver instance
